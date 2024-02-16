02e318e9d628169625663cb617c302ec9a4f63c1
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2024-02-16 18:02:03 +0530

    Time: 7 ms (97.49%), Space: 56.8 MB (61.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> res=new ArrayList<>();
-        List<String> path=new ArrayList<>();
-        helper(0,s,path,res);
+        List<List<String>> res = new ArrayList<>();
+        helper(0, s, new ArrayList<>(), res);
         return res;
     }
-    private void helper(int idx,String s,List<String> path, List<List<String>> res){
+
+    private void helper(int idx, String s, ArrayList<String> path, List<List<String>> res) {
         if(idx==s.length()){
             res.add(new ArrayList(path));
             return;
@@ -25,4 +25,5 @@ class Solution {
         }
         return true;
     }
+
 }
