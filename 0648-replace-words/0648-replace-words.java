92b62d7845e19694ad42e78c9db90c7404229895
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2024-06-07 18:39:46 +0530

    Time: 698 ms (11.1%), Space: 55.2 MB (69.48%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,17 +1,25 @@
 class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        String[] words = sentence.split(" ");
-        Collections.sort(dictionary);
-        // System.out.println(Arrays.toString(words));
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            for (String dic : dictionary)
-                if (words[i].startsWith(dic)) {
-                    words[i] = dic;
+    public String replaceWords(List<String> dict, String sentence) {
+        Set<String> s = new HashSet<>(dict);
+        StringBuilder res = new StringBuilder();
+        List<String> temp = new ArrayList<>(
+                Arrays.asList(sentence.split(" ")));
+
+        for (String t : temp) {
+            int i = 0;
+            while (i <= t.length()) {
+                String curr = t.substring(0, i++);
+                if (s.contains(curr)) {
+                    res.append(curr).append(" ");
                     break;
                 }
 
+                if (i == t.length() + 1)
+                    res.append(t).append(" ");
+            }
         }
-        return String.join(" ", words);
+
+        res.deleteCharAt(res.length() - 1);
+        return res.toString();
     }
 }
