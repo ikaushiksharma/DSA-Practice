7e7686f6182baf48e98b39309496fc676fc35aed
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2024-06-18 17:55:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,54 +1,25 @@
-///TC - O(NlogN) SC - O(N)
 class Solution {
-    class Pair {
-        int diff;
-        int prof;
-
-        Pair(int diff, int prof) {
-            this.diff = diff;
-            this.prof = prof;
-        }
-    }
-
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-
-        int n = difficulty.length;
-
-        Pair[] combine = new Pair[n];
-
-        for (int i = 0; i < n; i++) {
-            Pair p = new Pair(difficulty[i], profit[i]);
-            combine[i] = p;
+        int res = 0, j = 0, best = 0;
+        List<Pair<Integer, Integer>> temp = new ArrayList<>();
+        
+        for (int i = 0; i < worker.length; ++i) {
+            temp.add(new Pair<>(difficulty[i], profit[i]));
         }
 
-        Arrays.sort(combine, (a, b) -> Integer.compare(a.diff, b.diff));
+        // 2,4,6,8,10
+        //10,20,30,40,50
+        //4,5,6,7
 
+        temp.sort((a,b)->a.getKey()-b.getKey());
         Arrays.sort(worker);
-
-        int i = 0, j = 0, totalP = 0, prev = 0;
-
-        while (i < worker.length) {
-
-            while (j < n) {
-                int d = combine[j].diff;
-                int p = combine[j].prof;
-                // WE GOT A JOB , SAVE IT AND LOOK FOR BETTER JOB
-                if (d <= worker[i]) {
-                    prev = Math.max(p, prev);
-                }
-                /// IF JOB DIFFICULTY IS GREATER NO NEED TO GO FURTHER , WILL BREAK
-                if (d > worker[i]) {
-                    break;
-                }
+        for(int work:worker){
+            while(j<temp.size() && work>=temp.get(j).getKey() && best<=temp.get(j).getValue()){
+                best=temp.get(j).getValue();
                 j++;
             }
-            /// KEEP ON ADDING PROFITS HERE
-            totalP += prev;
-            i++;
+            res+=best;
         }
-
-        /// RETURN
-        return totalP;
-
+        return res;
     }
 }
