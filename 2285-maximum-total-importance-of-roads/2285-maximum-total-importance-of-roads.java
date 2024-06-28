bf837b4e8146707e8cc64dfd6048e092680e9333
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2024-06-28 23:42:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,27 +1,27 @@
 class Solution {
-    public long maximumImportance(int n, int[][] roads) {
-        ArrayList<Integer>[] adj=new ArrayList[n];
-        for(int i=0;i<n;i++) adj[i]=new ArrayList<Integer>();
-        for(int i=0;i<roads.length;i++){
-            adj[roads[i][0]].add(roads[i][1]);
-            adj[roads[i][1]].add(roads[i][0]);
+    public long maximumImportance(int n, int[][] road) {
+        ArrayList<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            adj[i] = new ArrayList<Integer>();
+        for (int i = 0; i < road.length; i++) {
+            adj[road[i][0]].add(road[i][1]);
+            adj[road[i][1]].add(road[i][0]);
         }
-        int importance[]=new int[n];
-        PriorityQueue<Integer> pq=new PriorityQueue<Integer>((a,b)->
-            adj[a].size()==adj[b].size()?a-b:adj[a].size()-adj[b].size());
-        int imp=1;
-        for(int i=0;i<n;i++){
+        int importance[] = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(
+                (a, b) -> adj[a].size() == adj[b].size() ? a - b : adj[a].size() - adj[b].size());
+        int imp = 1;
+        for (int i = 0; i < n; i++) {
             pq.add(i);
         }
-        while(!pq.isEmpty()){
-            int node=pq.remove();
-            importance[node]=imp;
+        while (!pq.isEmpty()) {
+            int node = pq.remove();
+            importance[node] = imp;
             imp++;
         }
-        long ans=0;
-        for(int road[]:roads){
-            ans+=importance[road[0]];
-            ans+=importance[road[1]];
+        long ans = 0;
+        for(int i=0;i<n;i++){
+            ans+=importance[i]*adj[i].size();
         }
         return ans;
     }
