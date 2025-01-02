4cc7d80a8aed74b8217c43a5c7bcb2121935b67b
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2025-01-02 18:32:43 +0530

    Time: 15 ms (11.16%), Space: 86.9 MB (20.5%) - LeetHub

---
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.java
@@ -0,0 +1,31 @@
+class Solution {
+    private final char[] VOWELS = { 'a', 'e', 'i', 'o', 'u' };
+
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int prefixArr[] = new int[words.length+1];
+        for (int i = 0; i < words.length; i++) {
+            prefixArr[i+1] = prefixArr[i];
+            if (isValid(words[i])) {
+                prefixArr[i+1]++;
+            }
+        }
+        System.out.println(Arrays.toString(prefixArr));
+        int res[]=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            res[i]=prefixArr[queries[i][1]+1]-prefixArr[queries[i][0]];
+        }
+        return res;
+    }
+
+    private boolean isValid(String word) {
+        boolean firstVowel = false;
+        boolean lastVowel = false;
+        for (char c : VOWELS) {
+            if (c == word.charAt(0))
+                firstVowel = true;
+            if (c == word.charAt(word.length() - 1))
+                lastVowel = true;
+        }
+        return firstVowel && lastVowel;
+    }
+}
