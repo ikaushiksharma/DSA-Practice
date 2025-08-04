1077d6b5ca62ced1516171c0ca08940947354b28
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2025-08-05 01:22:27 +0530

    Time: 48 ms (81.14%), Space: 54.5 MB (97.58%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,25 +1,17 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        int left = 0;
-        int res = 0;
-        for (int right = 0; right < fruits.length; right++) {
-            mp.put(fruits[right], mp.getOrDefault(fruits[right], 0) + 1);
-            while (mp.size() > 2) {
-                mp.put(fruits[left], mp.get(fruits[left]) - 1);
-                if (mp.get(fruits[left]) == 0)
-                    mp.remove(fruits[left]);
+        int left=0;
+        int res=0;
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int right=0;right<fruits.length;right++){
+            mp.put(fruits[right],mp.getOrDefault(fruits[right],0)+1);
+            while(mp.size()>2){
+                mp.put(fruits[left],mp.get(fruits[left])-1);
+                if(mp.get(fruits[left])==0) mp.remove(fruits[left]);
                 left++;
             }
-            res = Math.max(res, getSumOfFrequencies(mp));
+            res=Math.max(res,right-left+1);
         }
-        return res;
-    }
-
-    private int getSumOfFrequencies(HashMap<Integer, Integer> mp) {
-        int res = 0;
-        for (int cnt : mp.values())
-            res += cnt;
         return res;
     }
 }
