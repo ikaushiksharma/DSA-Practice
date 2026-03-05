276b676b3a04c40da329367afc5c90fe10ef92b4
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2026-03-05 18:41:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.c
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.c
@@ -1,15 +1,9 @@
-int minOperations(char * s){
-    int len = strlen(s);
-    int beginOne = 0, beginZero = 0;
-    for(int i = 0; i < len; i++){
-        if(i & 1){
-            beginOne += (s[i]=='1')? 1 : 0;
-            beginZero += (s[i]=='0')? 1 : 0;
-        }
-        else {
-            beginOne += (s[i]=='0')? 1 : 0;
-            beginZero += (s[i]=='1')? 1 : 0;
-        }        
+class Solution {
+    public int minOperations(String s) {
+        int count = 0, n = s.length();
+        for (int i = 0; i < n; i++)
+            count += (s.charAt(i) ^ i) & 1;
+          
+        return Math.min(count, n - count);
     }
-    return beginOne<beginZero? beginOne :beginZero;
 }
